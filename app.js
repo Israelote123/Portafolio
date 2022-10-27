cbe67e2d0c81a6d584576f4d138fd3326bfb3f11
Israelote123 <joseprinsisrael@gmail.com>
2022-10-27 15:37:40 -0500

    responsive

---
+++ app.js
@@ -0,0 +1,8 @@
+const typed = new Typed('.typed', {
+    strings: ['Web', 'Full Stack', 'Front End', 'Back End'],
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    backDelay: 1500,
+    loop: true
+})
